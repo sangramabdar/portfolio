26431989788985eb2b15e01257088c7d1074692a
sangramabdar <abdarsangram2697@gmail.com>
2023-04-17 22:48:03 +0530

    home updated

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -63,8 +63,8 @@ function Home() {
         </h2>
 
         <p className="mt-3">
-          A Self-driven, quick starter, passionate programmer with a curious
-          mind who enjoys solving a complex and challenging real-world problems.
+          A Self-driven, passionate programmer with a curious mind who enjoys
+          solving complex and challenging real-world problems.
         </p>
       </div>
     </Section>
