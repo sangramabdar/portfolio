cfea1f239f3be8e0a90625e279fe69707b83dc8d
sangramabdar <abdarsangram2697@gmail.com>
2024-02-22 22:40:40 +0530

    latest build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // images: { unoptimized: true },
+  images: { unoptimized: true },
   // output: "export",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
